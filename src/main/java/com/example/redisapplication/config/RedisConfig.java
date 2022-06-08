6c0f9b90eb0509630b9e0d5f1e5b556ef47e2c0c
Belet <oguzhan.belet@solviads.com>
2022-06-08 11:34:28 +0300

    changes jedis to lettuce

--- src/main/java/com/example/redisapplication/config/RedisConfig.java
+++ src/main/java/com/example/redisapplication/config/RedisConfig.java
@@ -1,24 +1,37 @@
 package com.example.redisapplication.config;
 
+import lombok.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
 @EnableRedisRepositories
 public class RedisConfig {
+/*
+
+
+    @Value("${inmemorydb.redis.hostname:null}")
+    private String hostName;
+
+    @Value("${inmemorydb.redis.port:null}")
+    private Integer port;
+*/
 
     @Bean
-    JedisConnectionFactory jedisConnectionFactory(){
-        return new JedisConnectionFactory();
+    public LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration());
     }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(){
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
+        template.setConnectionFactory(redisConnectionFactory());
         return template;
     }
 
